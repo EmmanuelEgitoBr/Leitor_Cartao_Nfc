031b2781289940c42344618e3219524da8bfd783
Stanislaw Cruz <techway.stanislaw@odontosystem.com.br>
2025-01-16 18:42:24 -0300

    Montagem da estrutura da API

---
+++ BackEnd/DigitalNfcCardReader/DigitalNfcCardReader.Api/Controllers/v1/NfcCard/NfcCardController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalNfcCardReader.Api.Controllers.v1.NfcCard
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NfcCardController : ControllerBase
+    {
+    }
+}
---
+++ BackEnd/DigitalNfcCardReader/DigitalNfcCardReader.Infra.Data.Queries/v1/NfcCard/GetNfcInfo/GetNfcInfoQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalNfcCardReader.Infra.Data.Queries.v1.NfcCard.GetNfcInfo
+{
+    internal class GetNfcInfoQuery
+    {
+    }
+}
