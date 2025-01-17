5a78bf38a1b63df100324621ecf21fb05761056f
Emmanuel Egito Oliveira Candeira Marinho <e_egito@yahoo.com>
2025-01-16 21:40:06 -0300

    Criação da entidade no domínio

---
+++ BackEnd/DigitalNfcCardReader/DigitalNfcCardReader.Domain/Entities/v1/NfcData.cs
@@ -0,0 +1,16 @@
+﻿using DigitalNfcCardReader.Domain.Fixed;
+
+namespace DigitalNfcCardReader.Domain.Entities.v1
+{
+    public class NfcData
+    {
+        public long TagId {  get; set; }
+        public string CardSerialCode { get; set; } = string.Empty;
+        public string? PatientName { get; set; }
+        public DateTime CardDeliveryDate { get; set; } = DateTime.Now;
+        public NfcStatusType NfcStatus { get; set; }
+        public DateTime CardUpdateDate { get; set; } = DateTime.Now;
+        public string? ModifierUserCode { get; set; }
+
+    }
+}
---
+++ BackEnd/DigitalNfcCardReader/DigitalNfcCardReader.Domain/Fixed/NfcStatusType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace DigitalNfcCardReader.Domain.Fixed
+{
+    public enum NfcStatusType
+    {
+        [Description("Ativo")]
+        Active,
+
+        [Description("Inativo")]
+        Inactive,
+
+        [Description("Bloqueado")]
+        Blocked
+    }
+}
